d5936b7af66fe67e2efa3d552be2cb0983fe2b3e
Todd Gerspacher <tgerspacher@dimpledough.com>
2012-04-24 11:25:30 -0400

    created RSpecGreeter class in greeter_spec.rb, spec now passes

--- greeter_spec.rb
+++ greeter_spec.rb
@@ -1,3 +1,9 @@
+class RSpecGreeter
+	def greet
+		"Hello RSpec!"
+	end
+end
+
 describe "RSpec Greeter" do 								# declare example group 
 	it "should say 'Hello RSpec!' when it receives the greet() message" do		# declare example using it method
 		greeter = RSpecGreeter.new						# the 'Given'
