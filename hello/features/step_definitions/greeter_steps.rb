43002d44690c253fb26289d6339c6e1613de5abe
Todd Gerspacher <tgerspacher@dimpledough.com>
2012-04-26 11:20:18 -0400

    update on hello project

--- hello/features/step_definitions/greeter_steps.rb
+++ hello/features/step_definitions/greeter_steps.rb
@@ -1,17 +1,17 @@
 class CucumberGreeter
-	def greet
-		"Hello Cucumber!"
-	end
+  def greet
+    "Hello Cucumber!"
+  end
 end
 
 Given /^a greeter$/ do
-	@greeter = CucumberGreeter.new
+  @greeter = CucumberGreeter.new
 end
 
 When /^I send it the greet message$/ do
-	@message = @greeter.greet
+  @message = @greeter.greet
 end
 
 Then /^I should see "([^"]*)"$/ do |greeting|
-	@message.should == greeting
+  @message.should == greeting
 end
