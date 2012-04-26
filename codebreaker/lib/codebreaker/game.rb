4c1caeaaed4195c83caed8075de1b4b862842820
Todd Gerspacher <tgerspacher@dimpledough.com>
2012-04-26 11:18:31 -0400

    update of current status, specs work

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -1,12 +1,22 @@
 module Codebreaker
+
   class Game
     def initialize(output)
-			@output = output
-		end
-  
-		def start
-			@output.puts 'Welcome to Codebreaker!'
-			@output.puts 'Enter guess:'
-		end
-	end
+      @output = output
+    end
+
+    def start(secret)
+      @secret = secret
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
+    end
+
+    def guess(guess)
+      marker = Marker.new(@secret, guess)
+
+      @output.puts '+' * marker.exact_match_count +
+                   '-' * marker.number_match_count
+    end
+  end
+
 end
